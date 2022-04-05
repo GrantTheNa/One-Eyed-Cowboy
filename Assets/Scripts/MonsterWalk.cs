bcd0923d60b718264dfbf1ec493b6c14fc883a58
GrantTheNa <82065825+GrantTheNa@users.noreply.github.com>
2022-04-05 23:15:17 +1000

    General Polishing to the sound functions

--- Assets/Scripts/MonsterWalk.cs
+++ Assets/Scripts/MonsterWalk.cs
@@ -1,13 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class MonsterWalk : MonoBehaviour
 {
     public Animator animator;
-
     public Monster MonsterScript; //Get player Script
-
     float velocity = 0.0f;
     public float acceleration = 0.01f;
     public float deceleration = 0.05f;
@@ -23,41 +20,36 @@ public class MonsterWalk : MonoBehaviour
     void Start()
     {
         MonsterScript.GetComponent<Monster>(); //get the player script reference.
-
-
         VelocityHash = Animator.StringToHash("Velocity");
-
     }
-
     // Update is called once per frame
     void Update()
     {
         //Walking
-        if (MonsterScript.velocity >= 0.5f && MonsterScript.velocity <= 1.5f)
+        if (MonsterScript.velocity >= 0.5f && MonsterScript.velocity <= 2.2f)
         {
             if (velocity! < 0.5f)
             {
                 velocity += Time.deltaTime * acceleration * 7;
-                //Debug.Log(velocity);
+                Debug.Log(velocity);
                 isWalking = true;
                 isRunning = false;
             }
         }
-
         //Running
-        if (MonsterScript.velocity >= 1.5f)
+        if (MonsterScript.velocity >= 2.2f)
         {
             if (velocity! < 1f)
             {
                 velocity += Time.deltaTime * acceleration * 7;
-                //Debug.Log(velocity);
+                Debug.Log(velocity);
                 isWalking = true;
                 isRunning = true;
             }
         }
 
-        //////walking sounds/////
-        if (isWalking == true && GetComponent<AudioSource>().isPlaying == false && playingSound == false)
+            //////walking sounds/////
+            if (isWalking == true && GetComponent<AudioSource>().isPlaying == false && playingSound == false)
         {
             if (isRunning == false)
             {
@@ -66,11 +58,8 @@ public class MonsterWalk : MonoBehaviour
             else if (isRunning == true)
             {
                 StartCoroutine(RunSound());
-
             }
-
         }
-
         IEnumerator WalkSound()
         {
             playingSound = true;
@@ -78,7 +67,6 @@ public class MonsterWalk : MonoBehaviour
             yield return new WaitForSeconds(1f);
             playingSound = false;
         }
-
         IEnumerator RunSound()
         {
             playingSound = true;
@@ -86,13 +74,11 @@ public class MonsterWalk : MonoBehaviour
             yield return new WaitForSeconds(0.15f);
             playingSound = false;
         }
-
-
         animator.SetFloat(VelocityHash, velocity);
-
         //Standing
         if (MonsterScript.velocity <= 0.1f && velocity > 0.0f)
         {
+            Debug.Log("StopWalking");
             velocity -= Time.deltaTime * deceleration * 4;
             isWalking = false;
         }
